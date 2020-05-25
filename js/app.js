bdee7ebc9bc045be8d9076b0ceebc83e6b341be0
segawa-g <54101256+segawa-g@users.noreply.github.com>
2020-05-25 18:53:04 +0900

    app.js更新2-1

--- js/app.js
+++ js/app.js
@@ -14376,10 +14376,10 @@ const checkImage = () => {
     // QRコードの読み取りに成功したらモーダル開く
     // 失敗したら再度実行
     if (code) {
-        //var newLine = zmaster.filter(function(item, index){
-            //if (item.抜き品番 == code.data.substr(3,10)) return true;
-          //});
-        openModal(code)
+        var newLine = zmaster.filter(function(item, index){
+            if (item.抜き品番 == code.data.substr(3,10)) return true;
+          });
+        openModal(code.data)
     } else {
         setTimeout(() => { checkImage() }, 200)
     }
